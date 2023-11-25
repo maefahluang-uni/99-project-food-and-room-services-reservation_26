28835d81e17b97ece91b1b545c5b35228c202a02
AnchisaSittee <6531503089@lamduan.mfu.ac.th>
2023-11-25 12:24:05 +0700

    กรี๊ดออกมา

--- src/main/java/th/mfu/repository/ListRoomrepository.java
+++
@@ -1,8 +0,0 @@
-package th.mfu.repository;
-
-
-import org.springframework.data.repository.CrudRepository;
-import th.mfu.domain.ListRoom;
-
-public interface ListRoomrepository extends CrudRepository<ListRoom,Long> {
-}
